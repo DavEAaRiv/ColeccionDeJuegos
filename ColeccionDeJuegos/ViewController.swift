098cce3f053e1c7d724169e8e6f5240f1d5b001d
DavidR <davidrivero092@gmail.com>
2020-12-11 21:17:08 -0800

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by DavRiv on 11/12/2020.
+//  Copyright © 2020 DavRiv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
