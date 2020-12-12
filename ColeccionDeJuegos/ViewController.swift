ef5020f3f0d88ae8eea620435427d80c835c2d7a
DavidR <davidrivero092@gmail.com>
2020-12-11 23:33:09 -0800

    Second commit, agregar fotos desde galería y mostrarlo en un tableView.

--- ColeccionDeJuegos/ViewController.swift
+++ ColeccionDeJuegos/ViewController.swift
@@ -1,20 +1,41 @@
-//
-//  ViewController.swift
-//  ColeccionDeJuegos
-//
-//  Created by DavRiv on 11/12/2020.
-//  Copyright © 2020 DavRiv. All rights reserved.
-//
-
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableVie: UITableView!
+    var juegos: [Juego] = [];
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad();
+        tableVie.dataSource = self;
+        tableVie.delegate = self;
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        let appDel = (UIApplication.shared.delegate as! AppDelegate);
+        let contexto = appDel.persistentContainer.viewContext;
+        
+        do{
+            try juegos = contexto.fetch(Juego.fetchRequest());
+            tableVie.reloadData();
+        }
+        catch{
+            print("Error al cargar lista...");
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return juegos.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell();
+        let juego = juegos[indexPath.row];
+        cell.textLabel?.text = juego.titulo;
+        cell.imageView?.image = UIImage(data: (juego.imagen!));
+        return cell;
+    }
 }
 
